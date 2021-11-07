ec93439064c8c4e30d7c2848741a04b4dcc56c1e
Anton Kolmakov <anton@kolmakov.in>
2021-11-06 18:04:45 -0700

    Fix rubocop issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # TODO: replace with real domains when going into production
+    origins "*" # TODO: replace with real domains when going into production
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource "*",
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
